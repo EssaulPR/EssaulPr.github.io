894b7c5c4530900c7957a155f059d7f92ad797a1
EssMush <a173474@une.edu.mx>
2020-06-23 16:19:42 -0500

    servicios update

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,23 +1,40 @@
 // Botones
+// Identidad
 const nuestraIdentidad =document.querySelector("#btn-identity");
 const subNuestraIdentidad =document.querySelector("#sub-btn-identity");
+// servicios
+const services =document.querySelector("#btn-services");
+const subServices =document.querySelector("#sub-btn-services");
+// Home
 const home =document.querySelector("#home");
 // Para hacer el toggle
 // BOTON PARA SCROLL DESDE EL NAV
 nuestraIdentidad.addEventListener("click", (e) => {
     $("html, body").animate({
         scrollTop: $("#cont-identidad").offset().top
-    }, 1000)
+    }, 1500)
 });
 // BOTON PARA SCROLL DESDE LA IMAGEN
 subNuestraIdentidad.addEventListener("click", (e) => {
     $("html, body").animate({
         scrollTop: $("#cont-identidad").offset().top
-    }, 1000)
+    }, 1500)
 });
 // Para regresar a home
 home.addEventListener("click", (e) => {
     $("html, body").animate({
         scrollTop: $("#scroll-index").offset().top
-    }, 1000)
+    }, 1500)
+});
+// BOTON PARA SCROLL DESDE EL NAV
+services.addEventListener("click", (e) => {
+    $("html, body").animate({
+        scrollTop: $("#cont-services").offset().top
+    }, 1500)
+});
+// BOTON PARA SCROLL DESDE LA IMAGEN
+subServices.addEventListener("click", (e) => {
+    $("html, body").animate({
+        scrollTop: $("#cont-services").offset().top
+    }, 1500)
 });
